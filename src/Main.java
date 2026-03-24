a0f7de8b5971d236b96d40223068b473b53b2dcd
jafdel <b.cocozza@live.co.uk>
2026-03-24 17:24:11 +0000

    Added methods and arrayList

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,47 @@
 //public class Main {
     void main() {
-        final String _USERNAME = "jafdel";
-        final String _PASSWORD = "P455w0rd";
+        ArrayList<String> users = new ArrayList<>();
+        ArrayList<String> credentials = new ArrayList<>();
+        users.add("jafdel");
+        credentials.add("password");
+        users.add("thefuzzbuzz");
+        credentials.add("Password");
+        users.add("bcocozza");
+        credentials.add("P455w0rd");
+
         Scanner reader = new Scanner(System.in);
-        String username = "";
-        String password = "";
         byte attempts = 3;
         do {
             IO.print("Enter your username: ");
-            username = reader.nextLine();
+            String username = reader.nextLine();
             IO.print("Enter your password: ");
-            password = reader.nextLine();
-            if (username.equals(_USERNAME) && password.equals(_PASSWORD)) {
-                attempts = -1;
-                IO.println("Welcome to the club!");
-            } else if (!username.isEmpty() && !username.isBlank() && !password.isEmpty() && !password.isBlank()) {
-                IO.println("Nope!");
-                attempts--;
-                if (attempts > 0)
-                    IO.println("You have " + attempts + " more attempt(s).");
-                else
-                    IO.println("You have been locked out.");
+            String password = reader.nextLine();
+            if (isValid(username, password)) {
+                if (isMatch(username, password, users, credentials)) {
+                    attempts = -1;
+                    IO.println("Welcome to the club!");
+                } else {
+                    IO.println("Nope!");
+                    if (--attempts > 0) // attempts == 1 means last attempt
+                        IO.println("You have " + attempts + " more attempt(s).");
+                    else
+                        IO.println("You have been locked out.");
+                }
             } else
                 IO.println("Please ensure neither the username nor password fields are blank.");
         } while(attempts > 0);
+    }
+
+    boolean isMatch(String username, String password, ArrayList<String> users, ArrayList<String> credentials) {
+        for (int i=0; i<users.size(); i++) {
+            if (username.equals(users.get(i)) && password.equals(credentials.get(i))) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    boolean isValid(String username, String password) {
+        return (!username.isEmpty() && !username.isBlank() && !password.isEmpty() && !password.isBlank());
     }
 //}
